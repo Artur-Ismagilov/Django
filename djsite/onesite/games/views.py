2108c278d6b96760505fd444d12d3aa1c8cbd246
Turkan1961 <119409298+Turkan1961@users.noreply.github.com>
2023-04-05 23:28:26 +0500

    Slug

--- djsite/onesite/games/views.py
+++ djsite/onesite/games/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse, HttpResponseNotFound
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 
 from .models import *
 
@@ -34,15 +34,24 @@ def contact(reqest):
 def login(request):
     return HttpResponse("Авторизация")
 
-def show_post(request, post_id):
-    return HttpResponse(f"Отображение статьи с id = {post_id}")
+def show_post(request, post_slug):
+    post = get_object_or_404(Games, slug=post_slug)
+
+    context = {
+        'post': post,
+        'menu': menu,
+        'title': post.title,
+        'cat_selected': post.cat_id,
+    }
+
+    return render(request, 'games/post.html', context=context)
 
 def pageNotFound(request, exception):
     return HttpResponseNotFound('<h1>Страница не найдена</h1>')
 
 
-def show_category(request, cat_id):
-    posts = Games.objects.filter(cat_id=cat_id)
+def show_category(request, cat_slug):
+    posts = Games.objects.filter(cat__slug=cat_slug)
 
     if len(posts) == 0:
         raise Http404()
@@ -51,7 +60,7 @@ def show_category(request, cat_id):
         'posts': posts,
         'menu': menu,
         'title': 'Отображение по рубрикам',
-        'cat_selected': cat_id,
+        'cat_selected': cat_slug,
     }
 
     return render(request, 'games/index.html', context=context)
